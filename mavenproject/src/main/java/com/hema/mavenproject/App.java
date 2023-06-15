98653bb65f99c80d8eedad08d043885f6112c3f2
Pentahemanagapadmavthi <Penta.hemanagapadmavathi@citiustech.com>
2023-06-15 13:20:08 +0530

    Maven project to push to GitHub

---
+++ mavenproject/src/main/java/com/hema/mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.hema.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
